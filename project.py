ed75fe557f6dc2b6f2ef14b9a513e5232a6a5f9b
Irmak Sirer <irmak.sirer@datascopeanalytics.com>
2016-09-13 14:55:12 -0500

    changed default optimization hyperparameters for projection (slower but more stable)

--- project.py
+++ project.py
@@ -11,9 +11,9 @@ import tensorflow as tf
 from model import DCGAN
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--lr', type=float, default=0.01)
-parser.add_argument('--momentum', type=float, default=0.9)
-parser.add_argument('--nIter', type=int, default=1000)
+parser.add_argument('--lr', type=float, default=0.0001)
+parser.add_argument('--momentum', type=float, default=0.6)
+parser.add_argument('--nIter', type=int, default=2000)
 parser.add_argument('--imgSize', type=int, default=64)
 parser.add_argument('--lam', type=float, default=0.1)
 parser.add_argument('--checkpointDir', type=str, default='checkpoint')
